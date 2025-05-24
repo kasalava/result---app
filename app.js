53e197c942ddd86cf8793c7db69b8c3bbc74fe59
kasalava <kasalava86@gmail.com>
2025-05-24 23:13:26 +0500

    onclick

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
- 
+ const resultElement = document.getElementById('result')
+ const input1 = document.getElementById('input1')
+ const input2 = document.getElementById('input2')
+ const submitBtn = document.getElementById ('submit')
+ const sum = Number(input1.value1+input1.value2)
+ resultElement.textContent = sum
+ console.log(typeof  sum)
+ submitBtn.onclick = function(){
+    console.log('hello click')
+ }
